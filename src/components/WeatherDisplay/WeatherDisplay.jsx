4c3375f74cd13b0d19e6bd3f22db321bc6854fc1
Sif Et tabaa <sifbusinesswork@gmail.com>
2023-01-14 22:06:06 +0100

    icons added if its sunny,raining,snowing,etc...

--- src/components/WeatherDisplay/WeatherDisplay.jsx
+++ src/components/WeatherDisplay/WeatherDisplay.jsx
@@ -1,32 +1,48 @@
+import { WiDaySunny, WiNightClear, WiRain, WiSnow, WiWindy } from "react-icons/wi";
 import { useSelector } from "react-redux";
 
 const WeatherDisplay = () => {
-  const weather = useSelector((state) => state.weather.weather);
-  const status = useSelector((state) => state.weather.status);
-  const error = useSelector((state) => state.weather.error);
+    const weather = useSelector((state) => state.weather.weather);
+    const status = useSelector((state) => state.weather.status);
+    const error = useSelector((state) => state.weather.error);
 
-  if (status === "loading") {
-    return <p>Loading...</p>;
-  }
+    if (status === "loading") {
+        return <p>Loading...</p>;
+    }
 
-  if (status === "failed") {
-    return <p>Error: {error}</p>;
-  }
+    if (status === "failed") {
+        return <p>Error: {error}</p>;
+    }
 
-  if (status === "succeeded" && weather) {
-    
-    return (
-      <div>
-        <p>Temperature: {weather.main.temp}</p>
-        <p>Pressure: {weather.main.pressure}</p>
-        <p>Humidity: {weather.main.humidity}</p>
-        <p>Min temperature: {weather.main.temp_min}</p>
-        <p>Max temperature: {weather.main.temp_max}</p>
-      </div>
-    );
-  }
+    if (status === "succeeded" && weather) {
+        let icon;
 
-  return null;
+        if (weather.wind.speed > 10) {
+            icon = <WiWindy size={50} />;
+        } else if (weather.snow) {
+            icon = <WiSnow size={50} />;
+        } else if (weather.rain) {
+            icon = <WiRain size={50} />;
+        } else if (weather.weather[0].icon.endsWith("d")) {
+            icon = <WiDaySunny size={50} />;
+        } else {
+            icon = <WiNightClear size={50} />;
+        }
+
+        return (
+            <div>
+                {icon}
+                <p>Temperature: {weather.main.temp}</p>
+                <p>Pressure: {weather.main.pressure}</p>
+                <p>Humidity: {weather.main.humidity}</p>
+                <p>Min temperature: {weather.main.temp_min}</p>
+                <p>Max temperature: {weather.main.temp_max}</p>
+            </div>
+        );
+    }
+
+    return null;
 };
+
 
 export default WeatherDisplay;
