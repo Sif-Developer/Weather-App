c5077118a3a9844f229a241aa47b6a9821e13b1f
Sif Et tabaa <sifbusinesswork@gmail.com>
2023-01-15 01:16:38 +0100

    added some data, and trying to add next 5 days weather

--- src/components/WeatherDisplay/WeatherDisplay.jsx
+++ src/components/WeatherDisplay/WeatherDisplay.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import WeatherIcon from "./WeatherIcon/WeatherIcon";
+import "../WeatherDisplay/weatherDisplay.css";
 
 const WeatherDisplay = () => {
   const weather = useSelector((state) => state.weather.weather);
@@ -19,12 +20,39 @@ const WeatherDisplay = () => {
     return (
       <div>
         <WeatherIcon weather={weather} />
-        <p>Temperature: {weather.main?.temp}</p>
-        <p>Pressure: {weather.main?.pressure}</p>
-        <p>Humidity: {weather.main?.humidity}</p>
-        <p>Min temperature: {weather.main?.temp_min}</p>
-        <p>Max temperature: {weather.main?.temp_max}</p>
-        <p>Current date: {date.toLocaleDateString()}</p>
+        {weather?.name && weather?.sys?.country && <p>City: {weather.name}</p>}
+        {weather?.sys?.country && <p>Country Code: {weather.sys.country}</p>}
+        {weather?.weather?.[0]?.description && (
+          <p>Weather: {weather.weather[0].description}</p>
+        )}
+        {weather?.main?.pressure && <p>Pressure: {weather.main.pressure}</p>}
+        {weather?.main?.humidity && <p>Humidity: {weather.main.humidity}</p>}
+        {weather?.main?.temp && <p>Temperature: {weather.main.temp}</p>}
+        {weather?.main?.temp_min && (
+          <p>Min temperature: {weather.main.temp_min}</p>
+        )}
+        {weather?.main?.temp_max && (
+          <p>Max temperature: {weather.main.temp_max}</p>
+        )}
+        <p>
+          Current date:{" "}
+          {date.toLocaleDateString("en-EN", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </p>
+        <p>Forecast for next 4 days:</p>
+        {console.log(weather.daily)}
+        <ul>
+          {weather?.daily?.slice(0, 4).map((day, index) => (
+            <li key={index}>
+              Day {index + 1}: {day.weather[0].description}, Min Temp:{" "}
+              {day.temp.min}°C, Max Temp: {day.temp.max}°C
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
